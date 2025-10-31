95a010db910c0a60ccec51be216ed82c04376f8d
martin0martinase <leonidcernikov43@gmail.com>
2025-10-31 20:49:11 +0700

    add make(2)

--- main.cpp
+++ main.cpp
@@ -14,16 +14,41 @@ int main ()
     return 1;
   }
   int ** mtx = nullptr;
+  try
+  {
   mtx = make_mtx (rows, cols);
+  }
+  catch (const std::bad_alloc &)
+  {
+    return 2;
+  }
   output (mtx);
   rm (mtx, rows, cols);
 }
 
 void rm (int ** mtx, int r)
 {
-  for (size_t i = 0, i < r, ++i)
+  for (size_t i = 0; i < r; ++i)
   {
     delete[] mtx[i];
   }
   delete[] mtx;
+}
+
+int ** make (int r, int c)
+{
+  int ** mtx = new int * [r];
+  for (size_t i = 0; i < r; ++i)
+  {
+    try
+    {
+      mtx[i] = new int[c];
+    }
+    catch (const std::bad_alloc &)
+    {
+      rm (mtx,i);
+      throw;
+    }
+  }
+  return mtx;
 }
