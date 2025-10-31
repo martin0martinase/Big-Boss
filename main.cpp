c19c6b3a1158e5a42a8667a12743f66b19c293d5
martin0martinase <leonidcernikov43@gmail.com>
2025-10-31 20:19:34 +0700

    declarate program(1)

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int ** make_mtx (int r,int c);
+void  output (const int * const * mtx);
+void rm (int ** mtx);
+
+int main ()
+{
+  int rows = 0;
+  int cols = 0;
+  std::cin >> rows >> cols;
+  if (std::cin.fail())
+  {
+    return 1;
+  }
+  int ** mtx = nullptr;
+  mtx = make_mtx (rows, cols);
+  output (mtx);
+  rm (mtx);
+}
